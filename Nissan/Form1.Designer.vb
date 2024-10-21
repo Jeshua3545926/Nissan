fc7ee0823ec9a619101e36f26e5ab2cdd14dadba
garza <garza@ASUS>
2024-10-20 22:30:29 -0600

    Agregar archivos de proyecto.

---
+++ Nissan/Form1.Designer.vb
@@ -0,0 +1,252 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+		Me.Button1 = New System.Windows.Forms.Button()
+		Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+		Me.TextBox1 = New System.Windows.Forms.TextBox()
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.Panel1 = New System.Windows.Forms.Panel()
+		Me.Label2 = New System.Windows.Forms.Label()
+		Me.Label3 = New System.Windows.Forms.Label()
+		Me.Button2 = New System.Windows.Forms.Button()
+		Me.Label4 = New System.Windows.Forms.Label()
+		Me.TextBox2 = New System.Windows.Forms.TextBox()
+		Me.Label5 = New System.Windows.Forms.Label()
+		Me.TextBox3 = New System.Windows.Forms.TextBox()
+		Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+		Me.JeshuaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+		Me.ProductosToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+		Me.InicioDeSesionToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+		Me.RegistrarseToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+		Me.MenuStrip1.SuspendLayout
+		Me.SuspendLayout
+		'
+		'Button1
+		'
+		Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
+		Me.Button1.FlatAppearance.BorderSize = 0
+		Me.Button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red
+		Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+		Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+		Me.Button1.Location = New System.Drawing.Point(1827, -3)
+		Me.Button1.Name = "Button1"
+		Me.Button1.Size = New System.Drawing.Size(57, 50)
+		Me.Button1.TabIndex = 0
+		Me.Button1.Text = "X"
+		Me.Button1.UseVisualStyleBackColor = true
+		'
+		'ComboBox1
+		'
+		Me.ComboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+		Me.ComboBox1.FormattingEnabled = true
+		Me.ComboBox1.Location = New System.Drawing.Point(266, 604)
+		Me.ComboBox1.Name = "ComboBox1"
+		Me.ComboBox1.Size = New System.Drawing.Size(321, 28)
+		Me.ComboBox1.TabIndex = 1
+		'
+		'TextBox1
+		'
+		Me.TextBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight
+		Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+		Me.TextBox1.Location = New System.Drawing.Point(245, 194)
+		Me.TextBox1.Name = "TextBox1"
+		Me.TextBox1.Size = New System.Drawing.Size(342, 19)
+		Me.TextBox1.TabIndex = 2
+		'
+		'Label1
+		'
+		Me.Label1.AutoSize = true
+		Me.Label1.Font = New System.Drawing.Font("MingLiU_HKSCS-ExtB", 24!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+		Me.Label1.Location = New System.Drawing.Point(956, 9)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(164, 48)
+		Me.Label1.TabIndex = 3
+		Me.Label1.Text = "Nissan"
+		'
+		'Panel1
+		'
+		Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(64,Byte),Integer), CType(CType(0,Byte),Integer), CType(CType(0,Byte),Integer))
+		Me.Panel1.Dock = System.Windows.Forms.DockStyle.Left
+		Me.Panel1.Location = New System.Drawing.Point(0, 0)
+		Me.Panel1.Name = "Panel1"
+		Me.Panel1.Size = New System.Drawing.Size(239, 924)
+		Me.Panel1.TabIndex = 4
+		'
+		'Label2
+		'
+		Me.Label2.AutoSize = true
+		Me.Label2.Font = New System.Drawing.Font("Microsoft Yi Baiti", 22!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+		Me.Label2.Location = New System.Drawing.Point(257, 524)
+		Me.Label2.Name = "Label2"
+		Me.Label2.Size = New System.Drawing.Size(374, 44)
+		Me.Label2.TabIndex = 5
+		Me.Label2.Text = "Producto que escogera"
+		'
+		'Label3
+		'
+		Me.Label3.AutoSize = true
+		Me.Label3.Font = New System.Drawing.Font("Microsoft Yi Baiti", 24!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+		Me.Label3.Location = New System.Drawing.Point(245, 129)
+		Me.Label3.Name = "Label3"
+		Me.Label3.Size = New System.Drawing.Size(342, 48)
+		Me.Label3.TabIndex = 6
+		Me.Label3.Text = "Ingrese su nombre"
+		'
+		'Button2
+		'
+		Me.Button2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
+		Me.Button2.FlatAppearance.BorderSize = 0
+		Me.Button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gold
+		Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+		Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+		Me.Button2.Location = New System.Drawing.Point(309, 667)
+		Me.Button2.Name = "Button2"
+		Me.Button2.Size = New System.Drawing.Size(156, 54)
+		Me.Button2.TabIndex = 7
+		Me.Button2.Text = "Comprar"
+		Me.Button2.UseVisualStyleBackColor = true
+		'
+		'Label4
+		'
+		Me.Label4.AutoSize = true
+		Me.Label4.Font = New System.Drawing.Font("Microsoft Yi Baiti", 24!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+		Me.Label4.Location = New System.Drawing.Point(245, 262)
+		Me.Label4.Name = "Label4"
+		Me.Label4.Size = New System.Drawing.Size(596, 48)
+		Me.Label4.TabIndex = 9
+		Me.Label4.Text = "Ingrese la cantidad De Productos"
+		'
+		'TextBox2
+		'
+		Me.TextBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight
+		Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None
+		Me.TextBox2.Location = New System.Drawing.Point(245, 365)
+		Me.TextBox2.Name = "TextBox2"
+		Me.TextBox2.Size = New System.Drawing.Size(596, 19)
+		Me.TextBox2.TabIndex = 8
+		'
+		'Label5
+		'
+		Me.Label5.AutoSize = true
+		Me.Label5.Font = New System.Drawing.Font("Microsoft Yi Baiti", 24!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+		Me.Label5.Location = New System.Drawing.Point(245, 409)
+		Me.Label5.Name = "Label5"
+		Me.Label5.Size = New System.Drawing.Size(562, 48)
+		Me.Label5.TabIndex = 11
+		Me.Label5.Text = "Ingrese El billete donde pagara"
+		'
+		'TextBox3
+		'
+		Me.TextBox3.BackColor = System.Drawing.SystemColors.ButtonHighlight
+		Me.TextBox3.BorderStyle = System.Windows.Forms.BorderStyle.None
+		Me.TextBox3.Location = New System.Drawing.Point(245, 502)
+		Me.TextBox3.Name = "TextBox3"
+		Me.TextBox3.Size = New System.Drawing.Size(596, 19)
+		Me.TextBox3.TabIndex = 10
+		'
+		'MenuStrip1
+		'
+		Me.MenuStrip1.BackColor = System.Drawing.Color.Transparent
+		Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
+		Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.JeshuaToolStripMenuItem, Me.ProductosToolStripMenuItem, Me.InicioDeSesionToolStripMenuItem})
+		Me.MenuStrip1.Location = New System.Drawing.Point(239, 0)
+		Me.MenuStrip1.Name = "MenuStrip1"
+		Me.MenuStrip1.Size = New System.Drawing.Size(1636, 33)
+		Me.MenuStrip1.TabIndex = 22
+		Me.MenuStrip1.Text = "MenuStrip1"
+		'
+		'JeshuaToolStripMenuItem
+		'
+		Me.JeshuaToolStripMenuItem.Name = "JeshuaToolStripMenuItem"
+		Me.JeshuaToolStripMenuItem.Size = New System.Drawing.Size(78, 29)
+		Me.JeshuaToolStripMenuItem.Text = "jeshua"
+		'
+		'ProductosToolStripMenuItem
+		'
+		Me.ProductosToolStripMenuItem.Name = "ProductosToolStripMenuItem"
+		Me.ProductosToolStripMenuItem.Size = New System.Drawing.Size(110, 29)
+		Me.ProductosToolStripMenuItem.Text = "productos"
+		'
+		'InicioDeSesionToolStripMenuItem
+		'
+		Me.InicioDeSesionToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.RegistrarseToolStripMenuItem})
+		Me.InicioDeSesionToolStripMenuItem.Name = "InicioDeSesionToolStripMenuItem"
+		Me.InicioDeSesionToolStripMenuItem.Size = New System.Drawing.Size(149, 29)
+		Me.InicioDeSesionToolStripMenuItem.Text = "inicio de sesion"
+		'
+		'RegistrarseToolStripMenuItem
+		'
+		Me.RegistrarseToolStripMenuItem.BackColor = System.Drawing.SystemColors.Highlight
+		Me.RegistrarseToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+		Me.RegistrarseToolStripMenuItem.Name = "RegistrarseToolStripMenuItem"
+		Me.RegistrarseToolStripMenuItem.Size = New System.Drawing.Size(196, 34)
+		Me.RegistrarseToolStripMenuItem.Text = "registrarse"
+		'
+		'Form1
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(9!, 20!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.BackColor = System.Drawing.SystemColors.ButtonHighlight
+		Me.ClientSize = New System.Drawing.Size(1875, 924)
+		Me.Controls.Add(Me.Button1)
+		Me.Controls.Add(Me.Label1)
+		Me.Controls.Add(Me.MenuStrip1)
+		Me.Controls.Add(Me.Label5)
+		Me.Controls.Add(Me.TextBox3)
+		Me.Controls.Add(Me.Label4)
+		Me.Controls.Add(Me.TextBox2)
+		Me.Controls.Add(Me.Button2)
+		Me.Controls.Add(Me.Label3)
+		Me.Controls.Add(Me.Label2)
+		Me.Controls.Add(Me.Panel1)
+		Me.Controls.Add(Me.TextBox1)
+		Me.Controls.Add(Me.ComboBox1)
+		Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+		Me.MainMenuStrip = Me.MenuStrip1
+		Me.Name = "Form1"
+		Me.Text = "Form1"
+		Me.MenuStrip1.ResumeLayout(false)
+		Me.MenuStrip1.PerformLayout
+		Me.ResumeLayout(false)
+		Me.PerformLayout
+
+End Sub
+	Friend WithEvents Button1 As System.Windows.Forms.Button
+ Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+ Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+ Friend WithEvents Label1 As System.Windows.Forms.Label
+ Friend WithEvents Panel1 As System.Windows.Forms.Panel
+ Friend WithEvents Label2 As System.Windows.Forms.Label
+ Friend WithEvents Label3 As System.Windows.Forms.Label
+ Friend WithEvents Button2 As System.Windows.Forms.Button
+ Friend WithEvents Label4 As System.Windows.Forms.Label
+ Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+	Friend WithEvents Label5 As Label
+	Friend WithEvents TextBox3 As TextBox
+	Friend WithEvents MenuStrip1 As MenuStrip
+	Friend WithEvents JeshuaToolStripMenuItem As ToolStripMenuItem
+	Friend WithEvents ProductosToolStripMenuItem As ToolStripMenuItem
+	Friend WithEvents InicioDeSesionToolStripMenuItem As ToolStripMenuItem
+	Friend WithEvents RegistrarseToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ Nissan/Form1.vb
@@ -0,0 +1,118 @@
+﻿Public Class Form1
+
+	Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+	ComboBox1.Items.Clear()  
+		ComboBox1.Items.AddRange(New String() {
+									"nissan Skyline GTR4 $4000",
+									"nissan Shyline GTR5 $5000",
+									"nissan Corolla 2005 $7340",
+									"nissan March 2010 $10256"
+									})
+	'Panel1.BackColor = Color.FromArgb(100,0,0,0)'
+
+	End Sub
+Public sub Conditon()
+
+Dim billete As Integer   = TextBox3.Text 
+If  billete <= 4000 Then 
+	Dim BilleteMenor As String = MessageBox.Show("Su billete es menor del total Desea volver a ingresar su billete", "advertencia",	MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation)
+		If BilleteMenor = DialogResult.Yes Then
+			MsgBox(Funcion())
+		Else
+			MsgBox("Operacion /404 cancelada")
+
+		End If
+	Else	
+		MsgBox(F())
+	End If
+End sub
+	Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+		Dim Result As String = MessageBox.Show("Desea salir del Programa?", "advertencia", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation)
+		If Result = DialogResult.Yes Then
+			Application.Exit()
+		Else
+			MsgBox("Operacion /404 cancelada")
+		End If
+	End Sub
+	Private Sub Form1_Paint(sender As Object, e As PaintEventArgs) Handles MyBase.Paint
+		Dim g As Graphics = e.Graphics
+		Dim pen As New Pen(Color.Black, 2)
+		g.DrawLine(pen, TextBox1.Left, TextBox1.Bottom, TextBox1.Right, TextBox1.Bottom)
+		g.DrawLine(pen, TextBox2.Left, TextBox2.Bottom, TextBox2.Right, TextBox2.Bottom)
+		g.DrawLine(pen, TextBox3.Left, TextBox3.Bottom, TextBox3.Right, TextBox3.Bottom)
+	End Sub
+	Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+		If ComboBox1.SelectedItem Is Nothing Then
+			MsgBox("Por favor seleccione un producto.")
+			Return
+		End If
+		Dim Product As String = ComboBox1.SelectedItem.ToString
+		Dim Nombre As String = TextBox1.Text
+		Dim cantidad As Integer = TextBox2.Text
+		Dim array() As String = {"Su cantidad es"}
+		    MsgBox(array(0) &cantidad)
+If ComboBox1.SelectedItem = "nissan Shyline GTR5 $5000" then
+			Dim operacion As Integer = cantidad * 2321 
+			Dim Arrays() As String = {"su total es de"}
+			Dim billete As Integer = TextBox3.Text 
+			MsgBox(Arrays(0) & operacion)
+			If  billete <= 4000 Then 
+				Dim BilleteMenor As String = MessageBox.Show("Su billete es menor del total Desea volver a ingresar su billete", "advertencia", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation)
+					If BilleteMenor = DialogResult.Yes Then
+						MsgBox(Funcion())
+					Else
+						MsgBox("Operacion /404 cancelada")
+				End If
+			Else	
+				MsgBox("pago existoso")
+		End If
+	End If 
+If ComboBox1.SelectedItem = "nissan Shyline GTR4 $4000" then
+			Dim operacion As Integer = cantidad * 2002
+			Dim Arrays() As String = {"su total es de"}
+			Dim billete As Integer = TextBox3.Text 
+			MsgBox(Arrays(0) & operacion)
+			If  billete <= 4000 Then 
+				Dim BilleteMenor As String = MessageBox.Show("Su billete es menor del total Desea volver a ingresar su billete", "advertencia", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation)
+					If BilleteMenor = DialogResult.Yes Then
+						MsgBox(Funcion())
+					Else
+						MsgBox("Operacion /404 cancelada")
+				End If
+			Else	
+				MsgBox("pago existoso")
+		End If
+	End If 
+If ComboBox1.SelectedItem = "nissan Corolla 2005 $7340" Then 
+			Dim  operacion As integer = cantidad * 256721 
+			Dim Arrays() as String  = {"su total es de"}
+			MsgBox(Arrays(0) & operacion)
+			Conditon()
+	End If
+If ComboBox1.SelectedItem = "nnissan March 2010 $10256" Then 
+			Dim  operacion As integer = cantidad * 256721 
+			Dim Arrays() as String  = {"su total es de"}
+			MsgBox(Arrays(0) & operacion)
+			Conditon()
+		End If
+	End Sub
+Function Funcion() As String
+		Return "vuelva  a ingresae su billete"
+End Function
+	Private Sub Button2_MouseHover(sender As Object, e As EventArgs) Handles Button2.MouseHover
+		If Me.Button2.FlatAppearance.MouseOverBackColor = Color.Gold Then 
+			 Me.Button2.ForeColor = color.White 			
+		End If
+	End Sub
+		Private Sub Button2_MouseLeave(sender As Object, e As EventArgs) Handles Button2.MouseLeave
+			Me.Button2.ForeColor =  Color.Black 
+		End Sub
+Function F() As String
+		Return "Pago existoso"
+End Function
+
+	Private Sub ContextMenuStrip1_Opening(sender As Object, e As System.ComponentModel.CancelEventArgs) 
+ MessageBox.Show("El menú contextual está por abrirse.")
+	End Sub
+End Class
